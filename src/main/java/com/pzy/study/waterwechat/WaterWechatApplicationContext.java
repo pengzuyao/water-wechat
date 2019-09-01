ca5dd8d98e55ce652020f496e70d6fddf9135d58
pengzuyao <714019874@qq.com>
2019-09-01 11:42:22 +0800

    init

---
+++ src/main/java/com/pzy/study/waterwechat/WaterWechatApplicationContext.java
@@ -0,0 +1,18 @@
+package com.pzy.study.waterwechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Destription:
+ * Author: pengzuyao
+ * Time: 2019-09-01
+ */
+
+@SpringBootApplication
+public class WaterWechatApplicationContext {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterWechatApplicationContext.class ,args);
+    }
+}
--- src/test/java/com/pzy/study/waterwechat/WaterWechatApplicationTests.java
+++ src/test/java/com/pzy/study/waterwechat/WaterWechatApplicationTests.java
@@ -1,6 +1,7 @@
 package com.pzy.study.waterwechat;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
